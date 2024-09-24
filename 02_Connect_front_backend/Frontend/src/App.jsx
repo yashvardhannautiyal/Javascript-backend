0d33f830e3df229def066a95de3f3441201ccfa1
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-09-25 00:41:10 +0530

    making frontend

--- 02_Connect_front_backend/Frontend/src/App.jsx
+++ 02_Connect_front_backend/Frontend/src/App.jsx
@@ -4,30 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Chai and fullstack</h1>
+    <p>Jokes : {jokes.length}</p>
+
+    {
+      jokes.map((joke) =>{
+        <div key ={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      })
+    }
     </>
   )
 }
